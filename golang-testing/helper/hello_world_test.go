b1f970b5d846fe15d594c1374fb94d4d0ee4b35a
SyahrulBhudiF <syahrul4w@gmail.com>
2024-07-04 13:45:48 +0700

    feat: Implement Assertion and Require testing

--- golang-testing/helper/hello_world_test.go
+++ golang-testing/helper/hello_world_test.go
@@ -1,10 +1,24 @@
 package helper
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("John")
 	if result != "Hello John" {
 		t.Error("Expected Hello John, got ", result)
 	}
+}
+
+func TestHelloWolrdAssert(t *testing.T) {
+	result := HelloWorld("John")
+	assert.Equal(t, "Hello John", result, "Result must be Hello John")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("John")
+	require.Equal(t, "Hello John", result, "Result must be Hello John")
 }
