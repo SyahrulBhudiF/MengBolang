ff2b30f17eb02ca82ee0f7df00a90697bd895311
SyahrulBhudiF <syahrul4w@gmail.com>
2024-07-07 13:26:55 +0700

    feat: Goroutines

---
+++ golang-goroutines/goroutines_test.go
@@ -0,0 +1,18 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutines(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
